669dec0dc7389ebfb709c155a76868f43b8c25d6
bgtvalex <bgtvalex@ya.ru>
2026-09-11 23:21:09 +0300

    1.13.3. Встречи: Вертикальные заголовки

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,6 +2,6 @@
 <footer class="footer navbar bg-dark sticky-bottom center shadow">
 	
 	<a class="ver" href="{{ route('versions.index') }}">
-    v1.13.2
+    v1.13.3
   </a>
 </footer>
--- resources/views/visits_table.blade.php
+++ resources/views/visits_table.blade.php
@@ -9,12 +9,13 @@
 
 <table class="visits-table table table-striped table-bordered mt-1">
     <thead>
-        <tr>
-            <th>Персона</th>
+        <tr class="visits-table__first-child">
+            <th class="visits-table-th">Персона</th>
             @foreach ($columns as $meeting)
-                <th>
+                <th class="visits-table__data">
                     <a href="#" data-bs-toggle="tooltip" data-bs-html="true"
                     data-bs-title="{{ $meeting['vstrechiTheme'] }}<br>отв.{{ $meeting['otv_name'] }}">
+                        {{ $meeting['vstrechiTheme'] }}<br>
                         {{ $meeting['vstrechi_data'] }}
                     </a>
                 </th>
@@ -23,10 +24,10 @@
     </thead>
     <tbody>
         @foreach ($rows as $row)
-            <tr>
+            <tr class="visits-table__first-child">
                 <td>{{ $row['person_name'] }}</td>
                 @foreach ($columns as $m)
-                    <td class="text-center">{{ $row[$m['vstrechi_id']] ?? '' }}</td>
+                    <td class="vstr text-center">{{ $row[$m['vstrechi_id']] ?? '' }}</td>
                 @endforeach
             </tr>
         @endforeach
